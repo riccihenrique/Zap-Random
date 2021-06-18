f3a470f796b7ecd23fd3a0338a0ab3c1bd0c08cb
Henrique <ricci.henrique@outlook.com>
2021-06-18 17:53:57 -0300

    fix

--- index.mjs
+++ index.mjs
@@ -16,21 +16,20 @@ function getImage() { // get random image
 
     const client = await wa.create(); // connect with whatsapp web
     const contacts = await client.getAllContacts(); // get all contacts
-    const amigos = contacts.filter(c => c.formattedName.includes('Mor') /*|| c.formattedName.includes('Pistola')*/); // filter the contacts
+    const amigos = contacts.filter(c => c.name ? c.name.includes('Matias') : false); // filter the contacts
 
     const text = await axios.get('http://asdfast.beobit.net/api/?type=word&length=7'); // get a random text
     const textTranslated = await axios.get(`https://api.mymemory.translated.net/get?q=${text.data.text}!&langpair=la|pt-br`); // translate the text (because it's in latin)
 
     for(let i = 0; i < amigos.length; i++) {// Create a custom message for each contact filtered above
         const img = await jimp.read(url); // read img from Url
-        
         let newImg = await img.print(font28, 10, 10, "Bom dia"); // Add msg
         
         newImg = await img.print(font78, 3, 50, amigos[i].pushname + '...'); // Get the name in whatsapp
-        newImg = await newImg.print(font28, 100, 300, 'e bora cod'); // Add msg
+        newImg = await newImg.print(font28, 100, 300, 'e bom final de semana'); // Add msg
         
         const base64 = await newImg.getBase64Async(jimp.MIME_JPEG); // convert the image to base64
-        await client.sendFile(amigos[i].id._serialized, base64, 'Bom dia.jpg', textTranslated.data.responseData.translatedText); // Send the image
+        await client.sendFile(amigos[i].id, base64, 'Bom dia.jpg', textTranslated.data.responseData.translatedText); // Send the image
     }
 })()
 
